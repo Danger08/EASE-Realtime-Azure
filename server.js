d6dc78dfee156d052978e8d8392f92394c39e973
Danger08 <danielgabriel08@hotmail.com>
2016-07-06 18:52:23 +0800

    Commit End of Shift July 6 2016

--- server.js
+++ server.js
@@ -12,31 +12,36 @@ var mysql      = require('mysql');
 //   password : 'summer08',
 //   database : 'easetech'
 // });
-
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'chat'
+});
 
 //
-users = [];
-connection = [];
+
+var connectedUsers = [];
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/HTMLPage.html');
 });
 
 io.on('connection', function(socket){
-  io.emit('chat message' , 'a user conected');
-
 
   socket.on('disconnect', function(){
-  console.log('user disconnected');
+  console.log('user disconnected' + socket.id);
+  var elementPos = connectedUsers.map(function(x) { console.log(x); return x.username; }).indexOf(socket.id);
+  var objectFound = connectedUsers[elementPos];
+  console.log(objectFound);
   });
-
+  socket.on('user joined' , function(username){
+  console.log(username+ ' joined');
+  connectedUsers[username] = socket.id;
+  console.log(connectedUsers);
+  });
   socket.on('chat message' , function(msg){
     io.emit('chat message' , msg);
-    // var connection = mysql.createConnection({
-    //   host     : 'localhost',
-    //   user     : 'root',
-    //   password : 'root',
-    //   database : 'chat'
-    // });
+
     // connection.connect();
     // connection.query("INSERT INTO `try`(`msg`) VALUES ('" + msg + "')", function(err, rows, fields) {
     // });
